3fd8c05400c7b09d9555b594dfa0255d4e85034f
Gage Dietz <GSDietz@student.fullsail.edu>
2020-05-20 14:35:09 -0400

    Stuff

--- FBXExporter/FBXExporter.cpp
+++ FBXExporter/FBXExporter.cpp
@@ -15,7 +15,9 @@ int main()
 	// If you want to work in another project you'll need to store the textures as dds afterward in a similar path to the one you create  here
 	//Also it wont matter if the model is not textured by default
 	// so do something like I did in the following line
-	FBX_InitLoad("Assets/Malbers Animations/Monsters/Forest Golem/Models/Forest_Golem_Test.fbx", "ObjectData/ForestGolem_1.mesh");
+	SimpleMesh mesh;
+	FBX_InitLoad("Assets/Golem.fbx", "ObjectData/ForestGolem_2.mesh");
+	Load_FBX("ObjectData/ForestGolem_2.mesh", mesh);
 	//FBX_InitLoad("Assets/cube.fbx", "ObjectData/cube.mesh");
 	return 1;
 }
