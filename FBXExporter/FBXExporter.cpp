330c4f4ee974618f49cfe27867cb53594299c59b
Moises Carmona <MCarmona@student.fullsail.edu>
2020-05-26 18:06:26 -0400

    nothing important just comment uncomment

--- FBXExporter/FBXExporter.cpp
+++ FBXExporter/FBXExporter.cpp
@@ -34,10 +34,30 @@ int main()
 	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_HeavyHit_From_Right.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_HeavyHit_From_Right_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_HeavyHit_From_Right.animfile");
 	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_Idle.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_Idle_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_Idle.animfile");*/
 	
-	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_Idle_Stretch.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_Idle_Stretch_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_Idle_Stretch.animfile");
-	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_Look.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_Look_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_Look.animfile");
-	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_Look_Opp.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_Look_Opp_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_Look_Opp.animfile");
-	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_Run.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_Run_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_Run.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_Idle_Stretch.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_Idle_Stretch_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_Idle_Stretch.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_Look.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_Look_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_Look.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_Look_Opp.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_Look_Opp_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_Look_Opp.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_3/Golem_3_Run.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimMesh/Golem_3_Run_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_3/AnimFile/Golem_3_Run.animfile");
+
+
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Attack2.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Attack2_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Attack2.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Attack4.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Attack4_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Attack4.animfile");
+
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Attack5.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Attack5_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Attack5.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Born.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Born_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Born.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Die2.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Die2_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Die2.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Die3.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Die3_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Die3.animfile");
+
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_HeavyHit_From_Back.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_HeavyHit_From_Back_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_HeavyHit_From_Back.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_HeavyHit_From_Front.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_HeavyHit_From_Front_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_HeavyHit_From_Front.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_HeavyHit_From_Left.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_HeavyHit_From_Left_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_HeavyHit_From_Left.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_HeavyHit_From_Right.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_HeavyHit_From_Right_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_HeavyHit_From_Right.animfile");
+	//Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Idle.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Idle_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Idle.animfile");
+
+	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Idle_Stretch.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Idle_Stretch_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Idle_Stretch.animfile");
+	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Look.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Look_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Look.animfile");
+	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Look_Opp.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Look_Opp_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Look_Opp.animfile");
+	Anim_FBX_InitLoad("Assets/Completed_Animated_Fbx/Golem_4/Golem_4_Run.fbx", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimMesh/Golem_4_Run_Mesh.AnimMesh", "ObjectData/Completed_Animated_Mesh_Files/Golem_4/AnimFile/Golem_4_Run.animfile");
 
 
 
